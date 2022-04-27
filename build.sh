322690a67d6328e0daff9d657c71368d461747b5
Zhiwei Han <han@fortiss.org>
2022-04-27 07:08:51 +0000

    Update README.md, build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build . -t hanzw/cuda-ssh-torch:cuda114-python38-torch11
+docker build . -t 356255531/cuda-ssh-torch:cuda114-ssh-py38-torch11
 # prepare gpu support and increase share memory for dataloader
-docker run -d --gpus all -it --shm-size=128G -p 52022:22 -p 52066:6006  -p 5205:5000 --name zhiwei -v ~/WorkSpace:/root/WorkSpace -v /hdd/dataset:/root/Datasets fortiss/cuda_ssh_torch:cuda113_ssh_py38_torch
+docker run -d --gpus all -it --shm-size=128G -p 52022:22 -p 52066:6006  -p 5205:5000 --name zhiwei -v ~/WorkSpace:/root/WorkSpace -v /hdd/dataset:/root/Datasets 356255531/cuda-ssh-torch:cuda114-ssh-py38-torch11
